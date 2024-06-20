294ba90af7774795f5ebeea843fe4b56d01377b5
Bleizing <dimasz_97@yahoo.com>
2024-06-20 15:13:37 +0700

    Remove Unused Import

--- src/main/java/com/bleizing/pos/PosApplication.java
+++ src/main/java/com/bleizing/pos/PosApplication.java
@@ -1,12 +1,8 @@
 package com.bleizing.pos;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import com.bleizing.pos.model.User;
-import com.bleizing.pos.repository.UserRepository;
 
 @SpringBootApplication
 @EnableJpaAuditing
--- src/main/java/com/bleizing/pos/model/User.java
+++ src/main/java/com/bleizing/pos/model/User.java
@@ -1,6 +1,5 @@
 package com.bleizing.pos.model;
 
-
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
