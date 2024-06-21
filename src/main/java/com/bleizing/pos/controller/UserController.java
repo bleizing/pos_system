1178c406101fc94d33f3b199a074cd3d5d6f5c22
Bleizing <dimasz_97@yahoo.com>
2024-06-21 16:29:41 +0700

    Add Validation and Swagger

--- src/main/java/com/bleizing/pos/controller/UserController.java
+++ src/main/java/com/bleizing/pos/controller/UserController.java
@@ -3,18 +3,21 @@ package com.bleizing.pos.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bleizing.pos.dto.LoginRequest;
 import com.bleizing.pos.dto.LoginResponse;
 import com.bleizing.pos.service.UserService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
+@Tag(name = "User", description = "User Controller")
+@RestController
 @Slf4j
 @RequestMapping("/user")
 public class UserController {
@@ -22,7 +25,7 @@ public class UserController {
 	private UserService userService;
 	
 	@PostMapping("/login")
-	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+	public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
 		log.info("req = {}", request);
 		try {
 			return new ResponseEntity<>(userService.login(request), HttpStatus.OK);
--- src/main/java/com/bleizing/pos/dto/LoginRequest.java
+++ src/main/java/com/bleizing/pos/dto/LoginRequest.java
@@ -2,7 +2,7 @@ package com.bleizing.pos.dto;
 
 import java.io.Serializable;
 
-import jakarta.annotation.Nonnull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
@@ -14,9 +14,9 @@ public class LoginRequest implements Serializable {
 	 */
 	private static final long serialVersionUID = -5893080609203621147L;
 
-	@Nonnull
+	@NotBlank
 	private String email;
 	
-	@Nonnull
+	@NotBlank
 	private String password;
 }
