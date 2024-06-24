117a6ddb07c535ff7aa5c6d6ea25fb5947a5f865
Bleizing <dimasz_97@yahoo.com>
2024-06-24 11:27:57 +0700

    Add Auth Bearer Token Header

--- src/main/java/com/bleizing/pos/controller/StoreController.java
+++ src/main/java/com/bleizing/pos/controller/StoreController.java
@@ -12,7 +12,9 @@ import com.bleizing.pos.dto.GetStoreByCodeRequest;
 import com.bleizing.pos.dto.GetStoreByCodeResponse;
 import com.bleizing.pos.service.StoreService;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,13 +22,15 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 @RequestMapping("/store")
+@SecurityRequirement(name = "Authorization")
 public class StoreController {
 	@Autowired
 	private StoreService storeService;
 	
 	@PostMapping("/getByCode")
-	public ResponseEntity<GetStoreByCodeResponse> getStoreByCode(@Valid @RequestBody GetStoreByCodeRequest request) {
+	public ResponseEntity<GetStoreByCodeResponse> getStoreByCode(@Valid @RequestBody GetStoreByCodeRequest request, HttpServletRequest servletRequest) {
 		log.info("req = {}", request);
+		log.info("servlet req = {}", servletRequest.getHeader("Authorization"));
 		try {
 			return new ResponseEntity<>(storeService.getStoreByCode(request), HttpStatus.OK);
 		} catch (Exception e) {
